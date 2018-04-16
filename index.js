0d2892a8c3bdd260c1d36b791d8a15d8b85ff1cf
Robert Bengtsson <robert.bengtsson@mantissa.se>
2018-04-16 21:37:13 +0200

    Minor tweaks and cleanup

--- index.js
+++ index.js
@@ -6,56 +6,48 @@ var url = require('url');
 var app = express();
 
 //Fill sqlite with mock data
-db.serialize(function() {
-  db.run("CREATE TABLE User(Username, Password)");
-  var stmt = db.prepare("INSERT INTO User VALUES ('Admin','CorrectHorseBatteryStaple')");
-  stmt.run();
-  stmt.finalize();
-  var stmt = db.prepare("INSERT INTO User VALUES ('Nisse','Sommar2019')");
-  stmt.run();
-  stmt.finalize();
+db.serialize(function () {
+    db.run("CREATE TABLE User(Username, Password)");
+    var stmt = db.prepare("INSERT INTO User VALUES ('Admin','CorrectHorseBatteryStaple')");
+    stmt.run();
+    stmt.finalize();
 });
- 
 
-app.get('/',function(req,res) {
+
+app.get('/', function (req, res) {
     res.sendfile('index.html')
 });
 
-app.get('/login', function(req,res) {
+app.get('/login', function (req, res) {
     console.log(req.query.password);
-        GetAllUsers(req.query.username, req.query.password, function(isAuthenticated, query) {
-    
-        if (isAuthenticated)
-        {
-                res.sendfile('loggedIn.html')    
+    GetAllUsers(req.query.username, req.query.password, function (isAuthenticated, query) {
+
+        if (isAuthenticated) {
+            res.sendfile('loggedIn.html')
         }
-        else if (query)
-        {
-            res.send("ERROR! could not execute: "+ query)
+        else if (query) {
+            res.send("ERROR! could not execute sqlite query: " + query)
         }
-        else
-        {
+        else {
             res.redirect('/?WrongPassword=1');
         }
 
-        })
-    });
+    })
+});
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
 
 
-function GetAllUsers(username, password, authenticated)
-{
+function GetAllUsers(username, password, authenticated) {
     var result;
-    var query = "SELECT * FROM User WHERE Username = '" + username + "' and Password = '" + password +"'";
+    var query = "SELECT * FROM User WHERE Username = '" + username + "' AND Password = '" + password + "'";
     console.log(query);
-    db.all(query, function(err, row) 
-    {
-        if (err) 
+    db.all(query, function (err, row) {
+        if (err)
             authenticated(false, query)
-        else if (row.length>0)
-            authenticated(true) ;
+        else if (row.length > 0)
+            authenticated(true);
         else
-            authenticated(false) ;
-    } );
+            authenticated(false);
+    });
 }
